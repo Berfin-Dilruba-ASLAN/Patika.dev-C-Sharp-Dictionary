2454e943bc818c910a0509eee2064a1af92d255b
Berfin Dilruba ASLAN <98692629+Berfin-Dilruba-ASLAN@users.noreply.github.com>
2022-09-02 16:32:34 +0300

    Add files via upload

---
+++ dictionary/dictionary_hackerrank_sorusu/dictionary_hackerrank_sorusu/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        var n = int.Parse(Console.ReadLine());
+        Dictionary<string, int> telefonRehberi = new Dictionary<string, int>();
+        for (int i = 0; i < n; i++)
+        {
+            var giris = Console.ReadLine().Split(' ');
+            var isim = giris[0];
+            var numara = int.Parse(giris[1]);
+            telefonRehberi.Add(isim, numara);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            var isim = Console.ReadLine();
+            if (telefonRehberi.ContainsKey(isim))
+            {
+                int numara = telefonRehberi[isim];
+                Console.WriteLine(isim + "=" + numara);
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+        }
+
+    }
+}
